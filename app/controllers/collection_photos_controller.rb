c7d47d9314de3c86cd3302596134759f4dc34b1f
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2018-09-10 16:38:31 -0300

    Devise login authentication

---
+++ app/controllers/collection_photos_controller.rb
@@ -0,0 +1,5 @@
+class CollectionPhotosController < ActionController::Base
+	def new
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticate :user do
+ 	root to: 'collection_photos#new'
+  	get 'collection_photos/new', to: 'collection_photos#new', as: 'new_collection_photo'
+  end
 end
